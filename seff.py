6820cae9d2e7a5ae5ea2ddb567797fb55415488e
David <davidcking.inbox@gmail.com>
2025-05-02 09:55:36 -0600

    added top level parsing to seff.py

--- seff.py
+++ seff.py
@@ -10,17 +10,25 @@ def main():
 
     json_file = sys.argv[1]
     with open(json_file) as f:
-        job_data = json.load(f)
+        data = json.load(f)
 
-    # Create a SlurmJob from the JSON dictionary
-    job = SlurmJob.from_json(job_data)
+    meta = data.get("meta")
+    command = meta.get("command")
+    print(command)
+    Slurm = meta.get("Slurm")
+    print(Slurm)
 
-    # Get SEFF-style info
-    seff_info = job.seff_stats()
+    for job_data in data.get('jobs'):
 
-    # Print the SEFF fields
-    for key, value in seff_info.items():
-        print(f"{key}: {value}")
+        # Create a SlurmJob from the JSON dictionary
+        job = SlurmJob.from_json(job_data)
+
+        # Get SEFF-style info
+        seff_info = job.seff_stats()
+
+        # Print the SEFF fields
+        for key, value in seff_info.items():
+            print(f"{key}: {value}")
 
 if __name__ == "__main__":
     main()
