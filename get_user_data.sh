6c80da3b144567d55ea5351205c8c87d2c004623
David King <davidcking.inbox@gmail.com>
2025-05-08 15:46:57 -0600

    added JobName to sacct format and text_files/me_last_year.txt

--- get_user_data.sh
+++ get_user_data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sacct -P -n -a --format JobID,User,Group,State,Cluster,AllocCPUS,REQMEM,TotalCPU,Elapsed,MaxRSS,ExitCode,NNodes,NTasks $@
+sacct -P -n -a --format JobID,User,Group,State,Cluster,AllocCPUS,REQMEM,TotalCPU,Elapsed,MaxRSS,ExitCode,NNodes,NTasks,JobName $@
